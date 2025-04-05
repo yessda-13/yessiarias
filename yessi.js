fc39a8c9029be58d12a8e55530248e85b06ed637
yessda-13 <yessidaliav13@gmail.com>
2025-04-05 15:38:14 -0600

    Add files via upload

---
+++ yessi.js
@@ -0,0 +1,30 @@
+function saludar()
+{
+dejarnombre = inmediato("ingresa tu nombre");
+
+    si (nombre) 
+    {
+
+        alert("hola" + nombre + "¡suerte en tu examen")
+
+    }
+}
+
+function calcularCuadrado()
+{
+
+    dejarnumero = parseFloat(inmediato("ingresa un numero"));
+
+    si (!esNaN(numero))
+    {
+
+        dejarresultado = numero * numero;
+
+        alert("el cuadro de" + numero + " es: " + resultado); 
+    } demas
+    {
+        alert ("por favor, ingresa un numero valido.");
+    }
+
+
+}
